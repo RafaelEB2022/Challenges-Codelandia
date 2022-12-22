2a16575686e4362a2f71bee38ac0356b6999a009
RafaelEB2022 <rafael.silva@estrelabet.com>
2022-12-22 11:22:18 -0300

    Ajustado a responsividade para a versão desktop e adicionado o crédito ao Iuri Silva pelo código JavaScript

--- Desafio-001/src/main.js
+++ Desafio-001/src/main.js
@@ -1,3 +1,6 @@
+// Créditos ao Iuri Silva por disponibilizar este código.
+// Link para o CodePen: https://codepen.io/iuricode/pen/bGKjZxO
+
 const icon = document.querySelector("#icon");
 
 icon.onclick = () => {
